01d3a8ca4c77a1d7d0e48006c15269a1151d03e0
LivBenedicto <livia.benedicto@hotmail.com>
2021-01-12 17:35:51 -0300

    Adding routes

--- App.js
+++ App.js
@@ -1,23 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from "@react-navigation/native";
 
 import firebase from "./src/services/firebaseConnection";
+import Routes from './src/routes/index';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <NavigationContainer>
+      <Routes/>
       <StatusBar style="auto" />
-    </View>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
