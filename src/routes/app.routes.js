a99f9d7f8d934e3e001cc72be0f0d2eff8bf02ae
LivBenedicto <livia.benedicto@hotmail.com>
2021-01-12 17:03:54 -0300

    Adding routes

---
+++ src/routes/app.routes.js
@@ -0,0 +1,21 @@
+// routes login config
+
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import Home from "../pages/Home";
+
+const AppStack = createStackNavigator();
+
+function AppRoutes() {
+    return(
+        <AppStack.Navigator>
+            <AppStack.Screen
+                name="Home"
+                component={Home}
+            />
+        </AppStack.Navigator>
+    );
+}
+
+export default AppRoutes;
---
+++ src/routes/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import AuthRutes from "./auth.routes";
+// import AppRutes from "./app.routes";
+
+function Routes(){
+    return (
+        <AuthRutes/>
+    );
+}
+
+export default Routes;
